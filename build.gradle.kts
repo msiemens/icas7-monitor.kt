f06b97c8a7f5c1ed5cb8b8927c16c3bda806f046
Markus Siemens <markus@m-siemens.de>
2020-08-28 16:33:46 +0200

    test: add test for notification scheduling

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,22 +46,28 @@ graal {
 }
 
 dependencies {
+    implementation(kotlin("reflect:$kotlinVersion"))
     implementation("com.soywiz.korlibs.klock:klock:$klockVersion")
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-json:$ktorVersion")
     implementation("io.ktor:ktor-client-gson:$ktorVersion")
     implementation("com.google.code.gson:gson:2.8.6")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$serializationVersion")
     implementation("io.ktor:ktor-client-apache:$ktorVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3")
     implementation("io.github.microutils:kotlin-logging:1.8.3")
     implementation("org.slf4j:slf4j-jdk14:1.7.29")
+
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.6.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
 }
 
 tasks {
-    withType<KotlinCompile>() {
+    withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "11"
     }
 
@@ -75,6 +81,14 @@ tasks {
     withType<Jar> {
         manifest {
             attributes["Main-Class"] = application.mainClassName
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+
+        testLogging {
+            events("PASSED", "FAILED", "SKIPPED")
         }
     }
 
---
+++ src/test/kotlin/de/msiemens/icas7monitor/MainKtTest.kt
@@ -0,0 +1,86 @@
+package de.msiemens.icas7monitor
+
+import com.soywiz.klock.Date
+import com.soywiz.klock.DateTime
+import com.soywiz.klock.Time
+import com.soywiz.klock.hours
+import de.msiemens.icas7monitor.data.Auth
+import de.msiemens.icas7monitor.data.Course
+import de.msiemens.icas7monitor.data.State
+import de.msiemens.icas7monitor.data.Student
+import org.junit.jupiter.api.DisplayName
+import kotlin.test.*
+
+internal class MainKtTest {
+    val auth = Auth("", 1)
+    val course = Course(
+        courseId = "1",
+        startsOn = Date(2020, 1, 1),
+        endsOn = null,
+        startsAt = Time(16),
+        endsAt = Time(17),
+        students = listOf(
+            Student("DEU", "", "8GYM")
+        )
+    )
+
+    @Test
+    @DisplayName("skips when no changes")
+    fun `skips when no changes`() {
+        val state = State(
+            auth = auth,
+            courses = listOf(course.copy()),
+            queuedNotification = null
+        )
+
+        assertEquals(Action.SKIP, processChanges(listOf(course), state))
+    }
+
+    @Test
+    @DisplayName("skips when not scheduled notification")
+    fun `skips when not scheduled notification`() {
+        val state = State(
+            auth = auth,
+            courses = listOf(course.copy()),
+            queuedNotification = DateTime.now()
+        )
+
+        assertEquals(Action.SKIP, processChanges(listOf(course), state))
+    }
+
+    @Test
+    @DisplayName("queues notification when changes")
+    fun `queues notification when changes`() {
+        val state = State(
+            auth = auth,
+            courses = listOf(course.copy(endsAt = Time(18))),
+            queuedNotification = null
+        )
+
+        assertEquals(Action.QUEUE_NOTIFICATION, processChanges(listOf(course), state))
+    }
+
+    @Test
+    @DisplayName("queues notification when changes and scheduled")
+    fun `queues notification when changes and scheduled`() {
+        val state = State(
+            auth = auth,
+            courses = listOf(course.copy(endsAt = Time(18))),
+            queuedNotification = DateTime.now() - 2.hours
+        )
+
+        assertEquals(Action.QUEUE_NOTIFICATION, processChanges(listOf(course), state))
+    }
+
+    @Test
+    @DisplayName("sends notification when scheduled")
+    fun `sends notification when scheduled`() {
+        val state = State(
+            auth = auth,
+            courses = listOf(course.copy()),
+            queuedNotification = DateTime.now() - 2.hours
+        )
+
+        assertEquals(Action.SEND_NOTIFICATION, processChanges(listOf(course), state))
+    }
+}
